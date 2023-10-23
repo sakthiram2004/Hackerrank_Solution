d7e191c2e685d342705d605a7a436ff0143b6420
sakthiram2004 <your.email@example.com>
2023-10-23 21:04:08 +0530

    commited

---
+++ Compare the Triplets.c
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    b1=0
+    a1=0
+    for i,j in zip(a,b):
+        if i>j:
+            a1+=1
+        elif j>i:
+            b1+=1
+    r=[]
+    r.append(a1)
+    r.append(b1)
+    return r
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
